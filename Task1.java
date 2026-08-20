5af23fd1a511005259db1458ed43f0aa26d2fc77
vsangeetha326 <vsangeetha326@gmail.com>
2026-08-20 19:10:53 +0530

    First commit msg

--- Task1.java
+++ Task1.java
@@ -1,8 +1,10 @@
+package org.test.ExampleProject;
+
 public class Task1 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello welcome to hell");
+		System.out.println("Hello");
 		
 	}
 
